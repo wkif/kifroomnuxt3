81634edd144d798737e1c41b89a03d7020bc68e2
wkif <kif101001000@163.com>
2023-07-07 13:44:07 +0800

    feat：add aside

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div h="100vh" class="font-MiSans bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100">
     <NuxtLayout>
-      <NuxtPage p="t-7vh l-50px r-50px" h="93vh" />
+      <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <AppHeader h="6vh" />
-    <slot />
+    <div p="t-8vh l-50px r-50px" h="91vh" class="flex" flex="row">
+      <Aside w="20%" />
+      <div p="l-20%">
+        <slot />
+      </div>
+    </div>
     <AppFooter h="5vh" />
   </div>
 </template>
